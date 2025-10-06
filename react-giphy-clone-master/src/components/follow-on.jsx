9050ab74b46d23ae3398fd0296239cf6c91509fc
shaz-zack <786shahilahmed@gmail.com>
2025-10-06 20:23:08 +0530

    Updated features or fixed issues

--- react-giphy-clone-master/src/components/follow-on.jsx
+++ react-giphy-clone-master/src/components/follow-on.jsx
@@ -5,6 +5,18 @@ const FollowOn = () => {
     <div
       className="faded-text pt-2" //custom - faded-text
     >
+      <span>Follow on:</span>
+      <div className="flex gap-4 pt-3">
+        <a href="https://www.youtube.com/roadsidecoder">
+          <FaYoutube size={20} />
+        </a>
+        <a href="https://www.instagram.com/roadsidecoder">
+          <FaInstagram size={20} />
+        </a>
+        <a href="https://www.twitter.com/piyush_eon">
+          <FaXTwitter size={20} />
+        </a>
+      </div>
       
     </div>
   );
